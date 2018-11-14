2fc1a3e501c9b784b0f96b5d03d41b917ac8829b
Caleb Ogles <calebogles@MacBook-Pro-100.local>
2018-11-14 12:31:36 -0600

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Caleb Ogles on 11/14/18.
+//  Copyright © 2018 Caleb Ogles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
